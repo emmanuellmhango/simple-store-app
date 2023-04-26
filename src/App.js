4607581e5a3fa8f2070e502cea8b0bf35098e871
Emmanuel Mhango <emmanuellmhango@gmail.com>
2023-04-26 17:12:24 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Products from './products/Products';
@@ -8,18 +8,28 @@ import Cart from './cart/Cart';
 import Signup from './account/Signup';
 import Login from './account/Login';
 
-const App = () => (
-  <div className="App">
-    <Header />
-    <Routes>
-      <Route path="/" element={<Products />} />
-      <Route path="/categories" element={<Categories />} />
-      <Route path="/category/:id" element={<Specific />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  </div>
-);
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+  return (
+    <div className="App">
+      <Header  isLoggedIn={isLoggedIn} handleLogout={handleLogout} />
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/category/:id" element={<Specific />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
